1bc8e4dae75cf9886412b05c4663f8296f5b941c
Hwang-Jaeryeong <jryeong67@naver.com>
2024-01-24 05:29:09 +0900

    static 설정 추가

--- stadium/urls.py
+++ stadium/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf.urls.static import static
+from madcamp_week4_BE import settings
 from .views import StadiumAPIView, StadiumUpload
 
 urlpatterns = [
@@ -6,3 +8,6 @@ urlpatterns = [
     path('upload/', StadiumUpload.as_view(), name='stadium-upload'),
 ]
 
+
+if settings.DEBUG:
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
