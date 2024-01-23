0f366473302c644cd4ffa840d62662a2f8725100
Hwang-Jaeryeong <jryeong67@naver.com>
2024-01-23 17:26:59 +0900

    static 설정 추가

--- video/urls.py
+++ video/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+
+from madcamp_week4_BE import settings
 from .views import VideoUploadView
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('upload/', VideoUploadView.as_view(), name='video-upload'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
